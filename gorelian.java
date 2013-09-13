df0fdbe6a1d5fd7ab47c4955a6211fbb2092fbae
Max <shenfieldmax@gmail.com>
2013-09-13 09:44:10 -0500

    Create gorelian.java
    gorelian.java holds the xy coordinates of a gorelian and a radius of communication. Implemented by crash.java to solve crash_and_go.

---
+++ gorelian.java
@@ -0,0 +1,61 @@
+
+/**
+ * The gorelian class has xy coordinates and a radius representing the distance a radio can communicate
+ * Setter and getter methods for these properties.
+ * 
+ * @author Max Shenfield 
+ * @version 09/12/2013
+ */
+public class gorelian
+{
+    double xcoord;
+    double ycoord;
+    double radius;
+    /**
+     * Constructor for objects of class gorelian
+     */
+    public gorelian(int x, int y, int r)
+    {
+       xcoord = x;
+       ycoord = y;
+       radius = r;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public double getX()
+    {
+        return xcoord;
+    }
+    
+    public double getY()
+    {
+        return ycoord;
+    }
+    
+    public double getR()
+    {
+        return radius;
+    }
+    
+     public void setX(double x)
+    {
+        xcoord = x;
+    }
+    
+    public void setY(double y)
+    {
+        ycoord = y;
+    }
+    
+    public void setR(double r)
+    {
+        radius = r;
+    }
+    
+    
+}
