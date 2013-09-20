bda377bdbf8219252e280e2c18032ef04997cf87
Max <shenfieldmax@gmail.com>
2013-09-19 21:06:07 -0500

    Create Distance.java

---
+++ libraries/Distance.java
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Distance
+{
+
+    /**
+     * Constructor for objects of class Distance
+     */
+    public Distance()
+    {
+    }
+
+    public double betweenPoints(double x1, double x2, double y1, double y2)
+    {
+        return Math.sqrt(Math.pow((x1-x2),2) + Math.pow((y1-y2),2));
+    }
+}
